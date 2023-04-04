81f4dec21bdfa8a978cd80c919cacbc705ba078c
Aswini Veluchamy <45841530+Aswini-Veluchamy@users.noreply.github.com>
2023-04-04 13:38:14 +0530

    modified

--- kubeproject/settings.py
+++ kubeproject/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h(!3nm(35v!6x-87*r-79sfl+u_dj+5*o+9hbb(foo%f0t%3xx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
